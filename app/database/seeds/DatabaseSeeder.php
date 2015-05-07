c11c585310d5c40d44477e15272a82d5aa64bf16
Bryan Velastegui <bryan_slvr@hotmail.com>
2015-05-07 11:11:12 -0500

    Rellenamos nuestras tablas :book:

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -10,6 +10,41 @@ class DatabaseSeeder extends Seeder
 	public function run()
 	{
 		Eloquent::unguard();
-		// $this->call('UserTableSeeder');
+		DB::table('users')->delete();
+
+		$faker = \Faker\Factory::create('es_AR');
+
+		foreach(range(0, 10) as $index)
+		{
+			User::create([
+				'nombres' => $faker->firstName,
+				'apellidos' => $faker->lastName,
+				'direccion' => $faker->address,
+				'telefono' => $faker->phoneNumber,
+				'celular' => $faker->phoneNumber,
+				'email' => $faker->email,
+				'password' => 12345,
+			]);
+
+			Product::create([
+				'codigo' => $faker->uuid,
+				'marca' => $faker->company,
+				'modelo' => $faker->md5,
+				'cantidad' => $faker->numberBetween(1, 100),
+			]);
+		}
+
+		Role::create([
+			'name' => 'Administrator',
+			'detail' => 'Administrador de la tienda'
+		]);
+		Role::create([
+			'name' => 'Seller',
+			'detail' => 'Vendedor de la tienda'
+		]);
+		Role::create([
+			'name' => 'Client',
+			'detail' => 'Cliente de la tienda'
+		]);
 	}
 }
